0abbf5120170d310d8c74d07d2170e007a30f591
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2023-05-19 23:52:33 +0600

    Update Disjoint Set.java

--- Disjoint Set.java
+++ Disjoint Set.java
@@ -1,3 +1,38 @@
+class DisjointSet {
+    private int[] parent;
+    private int[] rank;
 
+    public DisjointSet(int size) {
+        parent = new int[size];
+        rank = new int[size];
 
+        // Initialize each element as a separate set with rank 0
+        for (int i = 0; i < size; i++) {
+            parent[i] = i;
+            rank[i] = 0;
+        }
+    }
 
+    public int find(int x) {
+        // Path compression: make the found root the parent of x
+        if (parent[x] != x)
+            parent[x] = find(parent[x]);
+
+        return parent[x];
+    }
+
+    public void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+
+        // Union by rank: attach the smaller rank tree under the root of the larger rank tree
+        if (rank[rootX] < rank[rootY])
+            parent[rootX] = rootY;
+        else if (rank[rootX] > rank[rootY])
+            parent[rootY] = rootX;
+        else {
+            parent[rootY] = rootX;
+            rank[rootX]++;
+        }
+    }
+}
