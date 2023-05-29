0bea3b09517d634f9810a09dcb06f626af6a6adc
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2023-05-29 17:22:12 +0600

    Update Bipartite Graph.java

--- Bipartite Graph.java
+++ Bipartite Graph.java
@@ -1,61 +1,99 @@
-import java.util.*;
+package dataStructure;
 
-class Solution
-{
-    private boolean dfs(int node, int col, int color[], 
-    ArrayList<ArrayList<Integer>>adj) {
-        
-        color[node] = col; 
-        
-        // traverse adjacent nodes
-        for(int it : adj.get(node)) {
-            // if uncoloured
-            if(color[it] == -1) {
-                if(dfs(it, 1 - col, color, adj) == false) return false; 
-            }
-            // if previously coloured and have the same colour
-            else if(color[it] == col) {
-                return false; 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class BipartiteGraph {
+    public static class Edges {
+        public int source;
+        public int destination;
+
+        public Edges(int source, int destination) {
+            this.source = source;
+            this.destination = destination;
+        }
+    }
+
+    private static class Graph {
+        public int vertices;
+        public LinkedList<Edges> adj[];
+
+        public Graph(int vertices) {
+            this.vertices = vertices;
+            adj = new LinkedList[vertices];
+
+            for (int i = 0; i < vertices; i++) {
+                adj[i] = new LinkedList<>();
             }
         }
-        
-        return true; 
-    }
-    public boolean isBipartite(int V, ArrayList<ArrayList<Integer>>adj)
-    {
-        int color[] = new int[V];
-	    for(int i = 0;i<V;i++) color[i] = -1; 
-	    
-	    // for connected components
-	    for(int i = 0;i<V;i++) {
-	        if(color[i] == -1) {
-	            if(dfs(i, 0, color, adj) == false) return false; 
-	        }
-	    }
-	    return true; 
-    }
-     public static void main(String[] args)
-    {
-        // V = 4, E = 4
-        ArrayList < ArrayList < Integer >> adj = new ArrayList < > ();
-        for (int i = 0; i < 4; i++) {
-            adj.add(new ArrayList < > ());
+
+        public void addEdges(int source, int destination) {
+            Edges edges = new Edges(source, destination);
+            adj[source].add(edges);
+
+            //// For an undirected graph, add the reverse edge as well
+            edges = new Edges(destination, source);
+            adj[destination].add(edges);
         }
-        adj.get(0).add(2);
-        adj.get(2).add(0);
-        adj.get(0).add(3);
-        adj.get(3).add(0);
-        adj.get(1).add(3);
-        adj.get(3).add(1);
-        adj.get(2).add(3);
-        adj.get(3).add(2);
-
-        Solution obj = new Solution();
-        boolean ans = obj.isBipartite(4, adj);
-        if(ans)
-            System.out.println("1");
-        else System.out.println("0");
     }
 
+    public void printGraph(LinkedList<Edges> adj[]) {
+        for (int i = 0; i < adj.length; i++) {
+            for (Edges edge : adj[i]) {
+                System.out.println(i + " Connected with " + edge.destination);
+            }
+        }
+    }
+
+    public boolean dfs(int node, int kind, int[] color, LinkedList<Edges> adj[]){
+        color[node] = kind;
+
+        for(Edges edge : adj[node]){
+            int u = edge.destination;
+
+            if(color[u] == -1) {
+                if(!dfs(u, 1-kind, color, adj)) return false;
+            } else if (color[u] == kind) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public boolean bipartite(int V, LinkedList<Edges> adj[]){
+        int[] color  = new int[V];
+        Arrays.fill(color, -1);
+
+        for(int i=0; i<V; i++){
+            if(color[i] == -1){
+                if(!dfs(i, 0, color, adj))  return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Graph graph = new Graph(9);
+
+        graph.addEdges(0,1);
+        graph.addEdges(1,2);
+        graph.addEdges(1,5);
+        graph.addEdges(2,3);
+        graph.addEdges(3,4);
+        graph.addEdges(4,8);
+        graph.addEdges(5,8);
+        graph.addEdges(3,6);
+        graph.addEdges(6,7);
+
+
+        BipartiteGraph bipartite = new BipartiteGraph();
+        bipartite.printGraph(graph.adj);
+
+        if(bipartite.bipartite(9, graph.adj)){
+            System.out.println("Graph is Bipartite");
+        }else{
+            System.out.println("Graph is not Bipartite");
+        }
+    }
 }
 
