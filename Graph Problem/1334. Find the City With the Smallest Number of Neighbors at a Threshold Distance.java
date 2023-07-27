337c3ea1bee7d0b8a3713c3a0afee90d9a1903e1
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2023-07-27 12:24:18 +0600

    Create 1334. Find the City With the Smallest Number of Neighbors at a Threshold Distance.java

---
+++ Graph Problem/1334. Find the City With the Smallest Number of Neighbors at a Threshold Distance.java
@@ -0,0 +1,91 @@
+class Solution {
+    public int findTheCity(int n, int[][] edges, int distanceThreshold) {
+        int[][] distance = new int[n][n];
+        
+        for(int i=0; i<n; i++){
+             for(int j=0; j<n; j++){
+                 if(i!=j) distance[i][j] = Integer.MAX_VALUE;
+             }
+        }
+        
+        for(int[] edge : edges){
+            distance[edge[0]][edge[1]] = edge[2];
+            distance[edge[1]][edge[0]] = edge[2];
+        }
+        
+        for(int k=0; k<n; k++){
+            for(int i=0; i<n; i++){
+                for(int j=0; j<n; j++){
+                    if(distance[i][k] != Integer.MAX_VALUE && distance[k][j] != Integer.MAX_VALUE && distance[i][k] + distance[k][j] < distance[i][j]){
+                        distance[i][j] = distance[i][k] + distance[k][j];
+                    }
+                } 
+            }
+        }
+        
+        int city = Integer.MAX_VALUE;
+        int resultantCity = 0;
+        
+        for(int i=0; i<n; i++){
+            int tempCity = 0;
+            for(int j=0; j<n; j++){
+                if(distance[i][j] <= distanceThreshold) tempCity++;
+            }
+            if(city == tempCity){
+                resultantCity = Math.max(resultantCity, i);
+            }else if(city > tempCity){
+                city = tempCity;
+                resultantCity = i;
+            }
+        }
+        return resultantCity;
+    }
+}
+
+
+//
+
+
+class Solution {
+    public int findTheCity(int n, int[][] edges, int distanceThreshold) {
+        int[][] distance = new int[n][n];
+        
+        for(int i=0; i<n; i++){
+             for(int j=0; j<n; j++){
+                 if(i!=j) distance[i][j] = Integer.MAX_VALUE;
+             }
+        }
+        
+        for(int[] edge : edges){
+            distance[edge[0]][edge[1]] = edge[2];
+            distance[edge[1]][edge[0]] = edge[2];
+        }
+        
+        for(int k=0; k<n; k++){
+            for(int i=0; i<n; i++){
+                for(int j=0; j<n; j++){
+                    if(distance[i][k] != Integer.MAX_VALUE && distance[k][j] != Integer.MAX_VALUE && distance[i][k] + distance[k][j] < distance[i][j]){
+                        distance[i][j] = distance[i][k] + distance[k][j];
+                    }
+                } 
+            }
+        }
+        
+        int city = Integer.MAX_VALUE;
+        int resultantCity = 0;
+        
+        for(int i=0; i<n; i++){
+            int tempCity = 0;
+            for(int j=0; j<n; j++){
+                if(distance[i][j] <= distanceThreshold) tempCity++;
+            }
+            if(tempCity <= city){
+                city = tempCity;
+                resultantCity = i;
+            }
+        }
+        
+        return resultantCity;
+    }
+}
+
